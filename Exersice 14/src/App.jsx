150ab5dd15477c3eadc5e34f8debffc133da7e7d
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-10-05 22:50:06 +0100

    finished

--- Exersice 14/src/App.jsx
+++ Exersice 14/src/App.jsx
@@ -1,6 +1,8 @@
 import MultipleStepform from './MultipleStepform'
+
 function App (){
   return (
     <MultipleStepform />
   )
 }
+export default App;
--- Exersice 14/src/MultipleStepform.jsx
+++ Exersice 14/src/MultipleStepform.jsx
@@ -1,82 +1,138 @@
+import { useReducer } from "react";
+
 const initialState = {
   step: 1, // Start at step 1
-  firstName: '',
-  lastName: '',
-  email: '',
-  phone: '',
-}
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+};
 
-const reducer = (state, action) =>{
-    switch(action.type){
-        case 'UpdateField':
-            return {
-                ...state,
-                [action.field]:action.value
-            }
-        case 'PrevField':
-            return {
-                
-            }
-        case 'ResetFeild':
-            return {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "UpdateField":
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case "PrevStep":
+      return {
+        ...state,
+        step: state.step - 1,
+      };
+    case "NEXT_STEP":
+      return {
+        ...state,
+        step: state.step + 1,
+      };
+    case "ResetFeild":
+      return initialState
+    default:
+      return state;
+  }
+};
 
-            }
-        default:
-            return state
-    }
-}
+const MultipleStepform = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-export const Profile = ()=>{
+  return (
+    <div>
+      {state.step === 1 && <Profile state={state} dispatch={dispatch} />}
+      {state.step === 2 && <Contant state={state} dispatch={dispatch} />}
+      {state.step === 3 && <Review state={state} dispatch={dispatch} />}
+    </div>
+  );
+};
+  const Profile = ({ state, dispatch }) => {
     return (
-        <div>
-            <h1>step 1 : profile</h1>
-            <label>firstName:</label>
-            <input 
-            type="text"
-             placeholder="Enter Your first-name"
-            />
-            <label>lastName:</label>
-            <input 
-            type="text"
-             placeholder="Enter Your Last-name"
-            />
-            <button>Next</button>
-        </div>
-    )
-}
+      <div>
+        <h1>step 1 : profile</h1>
+        <label>firstName:</label>
+        <input
+          type="text"
+          placeholder="Enter Your first-name"
+          value={state.firstName}
+          onChange={(e) =>
+            dispatch({
+              type: "UpdateField",
+              field: "firstName",
+              value: e.target.value,
+            })
+          }
+        />
+        <br></br>
+        <br></br>
+        <label>lastName:</label>
+        <input
+          type="text"
+          placeholder="Enter Your Last-name"
+          value={state.lastName}
+          onChange={(e) =>
+            dispatch({
+              type: "UpdateField",
+              field: "lastName",
+              value: e.target.value,
+            })
+          }
+        />
+        <br></br>
+        <button onClick={() => dispatch({ type: "NEXT_STEP" })}>Next</button>
+      </div>
+    );
+  };
 
-export const Contant = ()=>{
+  const Contant = ({ state, dispatch }) => {
     return (
-        <div>
+      <div>
         <h1>step 2 : Contant</h1>
-            <label>Email:</label>
-            <input 
-              type="Email"  
-             placeholder="Enter your Email"
-            />
-            <label>Phone:</label>
-            <input 
-                type="number"
-             placeholder="Enter Your Number"
-            />
-            <button>Back</button>
-            <button>Next</button>
-        </div>
-    )
-}
+        <label>Email:</label>
+        <input
+          type="Email"
+          placeholder="Enter your Email"
+          value={state.email}
+          onChange={(e) =>
+            dispatch({
+              type: "UpdateField",
+              field: "email",
+              value: e.target.value,
+            })
+          }
+        />
+        <label>Phone:</label>
+        <input
+          type="number"
+          placeholder="Enter Your Number"
+          value={state.phone}
+          onChange={(e) =>
+            dispatch({
+              type: "UpdateField",
+              field: "phone",
+              value: e.target.value,
+            })
+          }
+        />
+        <button onClick={() => dispatch({ type: "PrevStep" })}>Back</button>
+        <button onClick={() => dispatch({ type: "NEXT_STEP" })}>Next</button>
+      </div>
+    );
+  };
 
-export const Review = ()=>{
+  const Review = ({ state, dispatch }) => {
+    function handleConfrim() {
+      alert("the Confrim work seccesfully");
+      dispatch({ type: "ResetFeild" });
+    }
     return (
-        <div>
-            <h1>Step 3 : Review</h1>
-            <p>FirstName:{}</p>
-            <p>LastName:{}</p>
-            <p>Email:{}</p>
-            <p>Phone:{}</p>
+      <div>
+        <h1>Step 3 : Review</h1>
+        <p>FirstName:{state.firstName}</p>
+        <p>LastName:{state.lastName}</p>
+        <p>Email:{state.email}</p>
+        <p>Phone:{state.phone}</p>
 
-            <button>Back</button>
-            <button>Confrim</button>
-        </div>
-    )
-}
-
+        <button onClick={() => dispatch({ type: "PrevStep" })}>Back</button>
+        <button onClick={handleConfrim}>Confrim</button>
+      </div>
+    );
+  };
+  export default MultipleStepform;
