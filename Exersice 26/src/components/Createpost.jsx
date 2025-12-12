afee4b624e56357d9f649acb46a74b77c9522d21
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-12-12 11:03:43 +0000

    completed most ex

--- Exersice 26/src/components/Createpost.jsx
+++ Exersice 26/src/components/Createpost.jsx
@@ -1,29 +1,53 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { Postscontext } from "../context/Postscontext";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      postAdd({
+        title : isTitle, 
+        content :isContent});
+    setTitle('');
+    setContent('');
+  };
+  const { postAdd } = useContext(Postscontext);
+  const [isTitle, setTitle] = useState('');
+  const [isContent,setContent] = useState('');
+
   return (
     <div className="mt-6 p-4 rounded-lg shadow-md bg-white">
       <div className="mb-4 flex flex-col gap-6">
         <h1 className="font-bold text-2xl text-gray-600">Create a New Port</h1>
-        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <label>
             Title :
-            <input 
+            <input
               type="text"
-              className="border-gray-400 border rounded-sm" />
+              className="border-gray-400 border rounded-sm"
+              value={isTitle}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </label>
           <label>
             Content :
-            <textarea className="border-gray-400 border rounded-sm w-78 h-40"></textarea>
+            <textarea 
+            value={isContent}
+            onChange={(e) =>setContent(e.target.value)}
+            className="border-gray-400 border rounded-sm w-78 h-40"></textarea>
           </label>
-          <button className='bg-red-600 text-white border p-2 cursor-pointer rounded-lg'>Create</button>
+          <button
+            type="submit"
+            className="bg-red-600 text-white border p-2 cursor-pointer rounded-lg"
+          >
+            Create
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
